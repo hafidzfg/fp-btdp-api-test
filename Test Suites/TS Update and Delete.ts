dd9261e199dfad32852044487698ed9bd341a978
Hafidz Firmansyah Ghufara <hafidzfg@gmail.com>
2022-07-11 16:33:16 +0700

    commit test suite related to update and delete apis

---
+++ Test Suites/TS Update and Delete.ts
@@ -0,0 +1,71 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Test suites containing all test cases related to APIs with update method (PUT, PATCH) and delete (DELETE)</description>
+   <name>TS Update and Delete</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>3e14c7e5-c311-49b9-8d79-fccce53b4f68</testSuiteGuid>
+   <testCaseLink>
+      <guid>d18754f9-2403-4d47-9eb4-54e54f7076e3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Update APIs/PATCH Update User</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>false</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>29b9a2fb-ee34-476a-8f9b-d0d8cfd489ec</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>83e2dd26-5521-4d98-9f8a-a57372a1c4c5</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>d500e8a6-552b-4f7d-be86-c5ea9cb60dd1</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>43e8ebe4-3b6f-4feb-8543-1193d1f24463</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Update APIs/PUT Update User</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>false</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>53c93df1-2609-421a-a9d5-9982f1c0edbe</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>adbb81bd-32d0-4fa6-8490-8ab819dd3c07</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>6a597ec0-60b7-459f-b9c3-ddc941cd2fc9</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e3fab575-f3d8-47a9-9a99-dc62ead2358b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/DELETE APIs/DELETE a user</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
